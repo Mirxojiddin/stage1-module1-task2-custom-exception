316006c6a49ce69c6de88ffe1a66386d8e23a762
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-17 21:44:31 +0500

    Javob 1

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,20 +1,33 @@
 package com.epam.mjc;
 
 
-public class StudentManager {
+
+public class StudentManager extends Exception {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException {
+    if (studentID<11){
+      return Student.getValueOf(studentID);
+    }
+    else {
+      throw new StudentNotFoundException(
+          "Could not find student with ID " + studentID);
+    }
+    
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }
+    catch (StudentNotFoundException ex){
+        System.out.println(ex);
+    }
     }
 
   }
